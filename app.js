eecb48c9c669ab675853fe2118c8f7aef1d51d18
Lypzis <victorvp2008@hotmail.com>
2020-01-20 14:54:16 -0300

    Updating structure

--- app.js
+++ app.js
@@ -6,23 +6,7 @@ const app = express();
 // middleware, modifies incoming data
 app.use(express.json());
 
-// on get request to '/''
-// app.get('/', (req, res) => {
-//   // 200 = ok, just sending back a 'hello', right into the body
-//   //res.status(200).send('Hello from the server side :D');
-
-//   // sending back a json
-//   res
-//     .status(200) // 200 is default
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.status(200).send('You can POST now :D');
-// });
-
 const apiVersion = '/api/v1';
-
 const port = 3000;
 
 // file containing tours, reading the file outside for non-blocking reasons
@@ -30,8 +14,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`) // server will reload whenever this file gets modified
 ); // parsed to javascript object
 
-// sends back tours data to client
-app.get(`${apiVersion}/tours`, (req, res) => {
+const getAllTours = (req, res) =>
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -39,14 +22,9 @@ app.get(`${apiVersion}/tours`, (req, res) => {
       tours: tours
     }
   });
-});
 
-// ':id' the variable containing an id ... duh :D, the url can have multiple variables like this
-// for an optional variable: ':duration?'
-// the params are STRING, that is important to remember
-app.get(`${apiVersion}/tours/:id/:duration?`, (req, res) => {
+const getTour = (req, res) => {
   //console.log(req.params);
-
   const id = parseInt(req.params.id); // or req.params.id * 1 :D
   const tour = tours.find(el => el.id === id);
 
@@ -63,10 +41,9 @@ app.get(`${apiVersion}/tours/:id/:duration?`, (req, res) => {
       tour
     }
   });
-});
+};
 
-// receives data from client
-app.post(`${apiVersion}/tours`, (req, res) => {
+const createTour = (req, res) => {
   //console.log(req.body); // needs middleware, to work
   const newId = tours[tours.length - 1].id + 1; // the last tour id plus 1
   const newTour = Object.assign({ id: newId }, req.body); // inserts variable within object
@@ -86,10 +63,9 @@ app.post(`${apiVersion}/tours`, (req, res) => {
       });
     }
   ); // write it donw asynchronously into the file
-});
+};
 
-// for update data
-app.patch(`${apiVersion}/tours/:id`, (req, res) => {
+const updateTour = (req, res) => {
   // in case an invalid id is entered, this is just temporary sanitization
   if (req.params.id * 1 > tours.length - 1)
     return res.status(404).send({
@@ -105,9 +81,9 @@ app.patch(`${apiVersion}/tours/:id`, (req, res) => {
       tour: '<Updated tour here>'
     }
   });
-});
+};
 
-app.delete(`${apiVersion}/tours/:id`, (req, res) => {
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length - 1)
     return res.status(404).send({
       status: 'fail',
@@ -119,9 +95,28 @@ app.delete(`${apiVersion}/tours/:id`, (req, res) => {
     status: 'success',
     data: null
   });
-});
+};
+
+// EVENT HANDLERS /////////////////////////////
+
+// sends back tours data to client
+app.get(`${apiVersion}/tours`, getAllTours);
+
+// ':id' the variable containing an id ... duh :D, the url can have multiple variables like this
+// for an optional variable: ':duration?'
+// the params are STRING, that is important to remember
+app.get(`${apiVersion}/tours/:id/:duration?`, getTour);
+
+// receives data from client
+app.post(`${apiVersion}/tours`, createTour);
+
+// for update data
+app.patch(`${apiVersion}/tours/:id`, updateTour);
+
+app.delete(`${apiVersion}/tours/:id`, deleteTour);
 
 // starts the server listening;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`); // callback when starts listening
 });
+///////////////////////////////////////
