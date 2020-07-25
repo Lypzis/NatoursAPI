e612fe40ffb1cb38c384473d743f6bbea87fc4ea
Lypzis <victorvp2018@protonmail.com>
2020-07-25 14:29:22 -0300

    Implemented Stripe messages 2

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 // will run for every call to this route, there will(could) always be an special
 // alert in case of necessity.
-router.use(viewsController.alert);
+router.use(viewsController.alerts);
 
 //router.use(authController.isLoggedIn);
 
