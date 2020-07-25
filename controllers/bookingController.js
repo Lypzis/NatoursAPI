3125d7522f6712c476d339d0b47a2842deb34872
Lypzis <victorvp2018@protonmail.com>
2020-07-25 14:01:27 -0300

    Webhook bug fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -26,7 +26,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
+        ],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1
@@ -60,7 +62,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id; // set at the above 'getCheckoutSession';
   const user = (await User.findOne({ email: session.customer_email })).id; // only the id is wanted
-  const price = session.line_items[0].amount / 100; // back from the *100 conversion
+  const price = session.display_items[0].amount / 100; // back from the *100 conversion
 
   await Booking.create({ tour, user, price });
 };
